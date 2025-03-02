697d9f92b27b81dcf08395f5c7083530223d8e53
Rafael <108784198+raff0722@users.noreply.github.com>
2025-03-02 15:17:34 +0100

    Add files via upload

---
+++ twoD_deblurr_image/Problem_data/conf5/main.py
@@ -0,0 +1,40 @@
+# 128 x 128 section
+
+from pathlib import Path, PureWindowsPath
+
+from Localization.twoD_deblurr_image import problem, convolution, functions, maj_min_TV_deblurring
+
+# problem configuration
+conf_nr = '5'
+
+# parameters
+par = {
+    'image_file' : PureWindowsPath( r'twoD_deblurr_image\cameraman_512.tif' ),
+    'max_int' : 1,
+    'section': ((3*64,5*64), (3*64,5*64)),
+    'down_sam' : 1,
+    'seed' : 0,
+    'noise_spec' : 'std', #'BSNR', 
+    'noise_val' : 1e-2, # 40,
+    
+    'blur' : 'Gaussian',
+    'blur_std' : 8, # 4 in FISTA paper 
+    'radius' : 8, # 4 in FISTA paper
+    'ext_mode_blur' : 'zero', # reflect: cba|abcd|dcb  mirror: dcb|abcd|cba
+
+    'problem_dir' : PureWindowsPath( r'twoD_deblurr_image\Problem_data', 'conf'+conf_nr ),
+
+    'delta' : 35.80 
+}
+Path.mkdir(Path(par['problem_dir']), parents=True, exist_ok=True)
+functions.save(par['problem_dir'] / 'par', par)
+
+# create problem
+problem.main(par)
+
+# MAP (eps=0)
+x_im, y_im_true, y_im, lam, N, d = functions.load(par['problem_dir'] / 'problem')
+conv_op = lambda x: convolution.conv(x, x_im.shape, par['blur'], par['radius'], std=par['blur_std'], BC=par['ext_mode_blur'])
+conv_op_T = lambda y: convolution.conv_T(y, x_im.shape, par['blur'], par['radius'], std=par['blur_std'], BC=par['ext_mode_blur'])
+MAP_TV = maj_min_TV_deblurring.debl(functions.rav(y_im), conv_op, conv_op_T, par['ext_mode_blur'], 1/lam, mode='TV', max_int=par['max_int'], delta=par['delta'], it_MM_max=1_000_000, rel_diff_x_max=1e-5, it_CG_max=200, eps=0)
+functions.save(par['problem_dir'] / 'map_TV', MAP_TV)
---
+++ twoD_deblurr_image/Problem_data/conf5/sam_fM/main.py
@@ -0,0 +1,64 @@
+# sampling via "full" (global) MALA and eps=1e-5
+
+from pathlib import PureWindowsPath, Path
+from multiprocessing import Pool
+
+from Localization.twoD_deblurr_image.functions import load, save, rav
+from Localization import eval_samples
+from Localization.twoD_deblurr_image import sample_full_MALA_TV
+
+def paral_work_full_MCMC(input):
+    par, sa, ch_nr = input
+    sample_full_MALA_TV.main(par, sa, ch_nr)
+
+def main():
+
+    # problem and sampling configuration
+    conf_nr = '5'
+    sam_nr = 'fM' # "full MALA"
+
+    # parameters
+    sam = {
+
+        'acc_int_disp' : 100,
+
+        'N_po' : 2_000,
+        'N_save' : 200,
+        'th' : 200,
+        'N_b' : 125_000,
+        'n_ch' : 5, # number of chains
+
+        'h0' : 5e-06,
+        'M' : 10,
+        'adapt_step' : 1,
+        'tar_acc' : .547,
+
+        'eps' : 1e-5,
+
+        'sampling_dir' : PureWindowsPath( r'twoD_deblurr_image\Problem_data', 'conf'+conf_nr, 'sam_'+sam_nr ),
+    }
+    sam['x0'] = rav( load(PureWindowsPath(r'twoD_deblurr_image\Problem_data', 'conf'+conf_nr, 'map_TV')) )
+    save( Path( sam['sampling_dir'] / 'par'), sam)
+    
+    # tasks for CPUS
+    TASKS = []
+    par = load(PureWindowsPath(r'twoD_deblurr_image\Problem_data', 'conf'+conf_nr, 'par'))
+    for ii in range(sam['n_ch']):
+        TASKS.append((par, sam, ii))
+
+    # map to CPUs
+    # for task in TASKS:
+    #     paral_work_full_MCMC(task)
+    with Pool(processes=sam['n_ch']) as pool:
+        pool.map(paral_work_full_MCMC, TASKS)
+    pool.close()
+    pool.join()
+        
+    # evaluate samples
+    print('Eval samples...')
+    flags = {'mean':1, 'HDI':0, 'CI':1, 'ESS':1, 'rhat':1}
+    options = {'CI':0.90}
+    eval_samples.main(sam['sampling_dir'], sam['n_ch'], sam['N_po']//sam['N_save'], flags, options)
+
+if __name__ == '__main__':
+    main()

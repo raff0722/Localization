e6757c87cad8ca7f79813d7a061dbeb52b713985
Rafael <108784198+raff0722@users.noreply.github.com>
2025-03-02 15:16:06 +0100

    Add files via upload

---
+++ twoD_deblurr_image/Problem_data/conf6/sam_lM_fix/sample3.sh
@@ -0,0 +1,53 @@
+#!/bin/sh 
+
+### General options 
+
+#BSUB -R select[model==XeonE5_2650v4]
+
+### -- specify queue -- 
+#BSUB -q hpc
+
+### -- set the job Name --
+#BSUB -J 6_sam_lM_3
+
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 5
+
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+
+### -- specify that we need x GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=10GB]"
+
+### -- specify that we want the job to get killed if it exceeds x GB per core/slot -- 
+#BSUB -M 10GB
+
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 5:00
+
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+##BSUB -u your_email_address
+
+### -- send notification at start -- 
+#BSUB -B
+
+### -- send notification at completion -- 
+#BSUB -N
+
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Error_%J.err 
+
+# module load python3/3.11.4 #(3.9.12)
+# module load matplotlib/3.7.1-numpy-1.24.3-python-3.11.4
+# module load scipy/1.10.1-python-3.11.4
+
+### python3 -m pip install --user imageio
+### python3 -m pip install --user PyWavelets
+
+source /zhome/00/d/170891/Python/Localization/env/bin/activate
+python3 ../../../main_loc_MALA_parallel_TV.py cluster 6 lM 3 > 6_sam_lM_3.txt
+### deactivate
